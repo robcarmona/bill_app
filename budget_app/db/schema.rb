9bb1a7708530ac707c4ef26ddcff3495406cfd38
Robert Carmona <robertocarmona@Robertos-MacBook-Pro.local>
2016-01-28 20:48:20 -0500

    accounts model

--- budget_app/db/schema.rb
+++ budget_app/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160129014027) do
+ActiveRecord::Schema.define(version: 20160129014422) do
+
+  create_table "accounts", force: :cascade do |t|
+    t.string   "name",        limit: 255
+    t.string   "description", limit: 255
+    t.integer  "user_id",     limit: 4
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+  end
 
   create_table "bills", force: :cascade do |t|
     t.string   "name",          limit: 255
