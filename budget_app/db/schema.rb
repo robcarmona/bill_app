d9e1a789889e1846a7d6aa28ed82c2e930d7521f
Robert Carmona <robertocarmona@Robertos-MacBook-Pro.local>
2016-01-28 21:38:58 -0500

    update user to have password_digest

--- budget_app/db/schema.rb
+++ budget_app/db/schema.rb
@@ -61,11 +61,11 @@ ActiveRecord::Schema.define(version: 20160129014422) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",      limit: 255
-    t.string   "password",   limit: 255
-    t.string   "name",       limit: 255
-    t.datetime "created_at",             null: false
-    t.datetime "updated_at",             null: false
+    t.string   "email",           limit: 255
+    t.string   "password_digest", limit: 255
+    t.string   "name",            limit: 255
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
 end
